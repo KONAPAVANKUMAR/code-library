6839ef596858515119a3c300b031a107c8d72292
PAVAN KUMAR KONA <pavankumarkona@PAVANs-MacBook-Pro.local>
2021-06-24 15:19:00 +0530

    bit manipulation added

--- binary_xor.py
+++ binary_xor.py
@@ -1,10 +1,8 @@
 def binary_xor(a: int, b: int) -> str:
-  
     a_binary = str(bin(a))[2:] 
     b_binary = str(bin(b))[2:] 
 
     max_len = max(len(a_binary), len(b_binary))
-
     return "0b" + "".join(
         str(int(char_a != char_b))
         for char_a, char_b in zip(a_binary.zfill(max_len), b_binary.zfill(max_len))
